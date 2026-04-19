5cb3e694f5ddfe34951b2cb59fc02a4aa6306e04
vqa <vqa@cachyos>
2026-04-19 15:08:54 +0700

    fix main max height & table result width

--- src/component/layout/Main.jsx
+++ src/component/layout/Main.jsx
@@ -7,7 +7,7 @@ const Main = ({ setPeserta, setStep, step, peserta, type, setType }) => {
   const [finalResult, setFinalResult] = useState([]); 
 
   return (
-    <main className="px-4 md:px-12 min-h-dvh">
+    <main className="px-4 md:px-12 min-h-full">
       {step == 1 && <Tambah setPeserta={setPeserta} setStep={setStep} setType={setType} type={type} />}
       {step == 2 && <Hitung peserta={peserta} setPeserta={setPeserta} setStep={setStep} setFinalResult={setFinalResult} type={type} />}
       {step == 3 && <Result finalResult={finalResult} type={type}/>}
--- src/component/layout/Result.jsx
+++ src/component/layout/Result.jsx
@@ -66,13 +66,11 @@ const Result = ({ finalResult, type }) => {
       <Title teks={"Hasil Penilaian"} />
       <Regular teks={"Hasil akhir semua penilaian peserta"} />
       <div className="py-4 w-full">
-        <table className="w-full text-center  overflow-hidden rounded-lg text-base">
+        <table className="w-full text-center overflow-hidden rounded-lg text-base">
           <thead>
             <tr className="bg-blue-400 text-lg">
-              <th className="py-2 px-3 border-e-2 border-black max-w-fit lg:max-w-1">
-                No
-              </th>
-              <th className="py-2 px-3">Nama Peserta</th>
+              <th className="py-2 px-3 max-w-1 border-e border-base">No</th>
+              <th className="py-2 px-3 ">Nama Peserta</th>
               <th className="py-2 px-3">Nilai</th>
             </tr>
           </thead>
@@ -82,9 +80,7 @@ const Result = ({ finalResult, type }) => {
                 key={i.id}
                 className="even:bg-blue-400 odd:bg-blue-300 font-semibold"
               >
-                <td className="py-2 border-e-2 border-black max-w-fit lg:max-w-1">
-                  {j + 1}
-                </td>
+                <td className="py-2 px-3 border-e border-base text-center">{j + 1}</td>
                 <td className="text-left py-2 px-3">{i.nama}</td>
                 <td className="py-2 px-3">{i.nilaiFinal}</td>
               </tr>
